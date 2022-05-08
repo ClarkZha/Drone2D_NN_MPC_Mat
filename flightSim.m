e11c850e168c4325c3b16fbb24a224675e6b9ba4
Clark Zha <jiaming_zha@berkeley.edu>
2022-05-08 00:37:22 -0700

    add training and analysis code

--- flightSim.m
+++ flightSim.m
@@ -1,3 +1,13 @@
+dataSetNumber = 2;
+
+if dataSetNumber == 1
+    load('TrainingResult1.mat')
+elseif dataSetNumber == 2
+    load('TrainingResult2.mat')
+elseif dataSetNumber == 3
+    load('TrainingResult3.mat')
+end 
+
 simFlag = true;
 dt = 0.02; %[s]
 initPos = [0;0];
@@ -6,7 +16,7 @@ initPitch = 0;
 initPitchRate = 0;
 initState = [initPos;initVel;initPitch;initPitchRate];
 
-goalPos = [0.5;0.5];
+goalPos = [0.2;1.0];
 goalVel = [0;0];
 goalState = [goalPos;goalVel];
 
@@ -21,36 +31,133 @@ quadParam.grav = 9.81; %[m/s^2]
 quadParam.armLength = 0.1; %[m]
 quadParam.maxThrust = 3; %[N]
 
+stateSize = 8; %Size of input state
+actionSize = 2;
+actionHorizon = 2; %Output horizon of the NN controller
+
 horizon = 8;
 
-T = 1; %[s] end time
+T = 0.3; %[s] end time
 n = T/dt; %Steps to simulate
 
-stateRecord = zeros(6,n);
-commandRecord = zeros(2,n);
+stateRecord_MPC = zeros(6,n);
+stateRecord_NN = zeros(6,n);
+stateRecord_NN_MPC = zeros(6,n);
+commandRecord_MPC = zeros(2,n);
+commandRecord_NN = zeros(2,n);
+commandRecord_NN_MPC = zeros(2,n);
+
+
+% Simulate with MPC controller
 tRecord = zeros(1,n);
-stateRecord(:,1) = initState;
-currentState = initState;
 tRecord(1,1) = 0;
+stateRecord_MPC(:,1) = initState;
+currentState = initState;
+
+
 for i = 1:n-1 
     disp(tRecord(1,i));
     tRecord(1,i+1) = tRecord(1,i) + dt;
-    [command, ~] = droneMPC(dt, horizon, currentState, goalState, costParam, quadParam)
-    commandRecord(:,i) = command(:,1);
+    [command, ~] = droneMPC(dt, horizon, currentState, goalState, costParam, quadParam);
+    commandRecord_MPC(:,i) = command(:,1);
     nextState = stepDynamics(dt, currentState, command(:,1), quadParam, true);
-    stateRecord(:,i+1) = nextState;
+    stateRecord_MPC(:,i+1) = nextState;
     currentState = nextState;
 end
+
+
+% Simulate with NN controller
+stateRecord_NN(:,1) = initState;
+currentState = initState;
+for i = 1:n-1 
+    distX = goalState(1) - currentState(1);
+    distZ = goalState(2) - currentState(2);
+    initVel = currentState(3:4);
+    goalVel = goalState(3:4);
+    initPitch = currentState(5);
+    initPitchRate = currentState(6);
+    predInput = [distX, distZ, initVel',goalVel',initPitch,initPitchRate];
+    NN_output = predict(MPCNetObj, predInput);
+    NN_command = double(reshape(NN_output, [actionSize,actionHorizon]));
+    commandRecord_NN(:,i) = NN_command(:,1);
+    nextState = stepDynamics(dt, currentState, NN_command(:,1), quadParam, true);
+    stateRecord_NN(:,i+1) = nextState;
+    currentState = nextState;
+end
+
+
+% Simulate with NN-MPC controller
+stateRecord_NN_MPC(:,1) = initState;
+currentState = initState;
+for i = 1:n-1 
+    distX = goalState(1) - currentState(1);
+    distZ = goalState(2) - currentState(2);
+    initVel = currentState(3:4);
+    goalVel = goalState(3:4);
+    initPitch = currentState(5);
+    initPitchRate = currentState(6);
+    predInput = [distX, distZ, initVel',goalVel',initPitch,initPitchRate];
+    NN_output = predict(MPCNetObj, predInput);
+    NN_command = double(reshape(NN_output, [actionSize,actionHorizon]));
+    [NN_MPC_command, ~] = droneMPC(dt, horizon, initState, goalState, costParam, quadParam, NN_command);   
+    commandRecord_NN_MPC(:,i) = NN_MPC_command(:,1);
+    nextState = stepDynamics(dt, currentState, NN_MPC_command(:,1), quadParam, true);
+    stateRecord_NN_MPC(:,i+1) = nextState;
+    currentState = nextState;
+end
+
 figure
-plot(tRecord, stateRecord(1,:));
-figure
-plot(tRecord, stateRecord(2,:));
-figure
-plot(tRecord, stateRecord(3,:));
-figure
-plot(tRecord, stateRecord(4,:));
-figure
-plot(tRecord, stateRecord(5,:));
+subplot(3,2,1)
+plot(tRecord, stateRecord_MPC(1,:),'Color','#0072BD','LineWidth',2);
+hold on
+plot(tRecord, stateRecord_NN(1,:),'-o');
+plot(tRecord, stateRecord_NN_MPC(1,:),'-s');
+xlabel('t[s]')
+ylabel('pos-x [m]')
+AX=legend('MPC', 'NN', 'NN-MPC', 'location','northwest');
+
+subplot(3,2,2)
+plot(tRecord, stateRecord_MPC(2,:),'Color','#0072BD','LineWidth',2);
+hold on
+plot(tRecord, stateRecord_NN(2,:),'-o');
+plot(tRecord, stateRecord_NN_MPC(2,:),'-s');
+xlabel('t[s]')
+ylabel('pos-z [m]')
+
+
+subplot(3,2,3)
+plot(tRecord, stateRecord_MPC(3,:),'Color','#0072BD','LineWidth',2);
+hold on
+plot(tRecord, stateRecord_NN(3,:),'-o');
+plot(tRecord, stateRecord_NN_MPC(3,:),'-s');
+xlabel('t[s]')
+ylabel('vel-x [m/s]')
+
+subplot(3,2,4)
+plot(tRecord, stateRecord_MPC(4,:),'Color','#0072BD','LineWidth',2);
+hold on
+plot(tRecord, stateRecord_NN(4,:),'-o');
+plot(tRecord, stateRecord_NN_MPC(4,:),'-s');
+xlabel('t [s]')
+ylabel('vel-z [m/s]')
+
+
+subplot(3,2,5)
+plot(tRecord, stateRecord_MPC(5,:),'Color','#0072BD','LineWidth',2);
+hold on
+plot(tRecord, stateRecord_NN(5,:),'-o');
+plot(tRecord, stateRecord_NN_MPC(5,:),'-s');
+xlabel('t[s]')
+ylabel('pitch [rad]')
+
+subplot(3,2,6)
+plot(tRecord, stateRecord_MPC(6,:),'Color','#0072BD','LineWidth',2);
+hold on
+plot(tRecord, stateRecord_NN(6,:),'-o');
+plot(tRecord, stateRecord_NN_MPC(6,:),'-s');
+xlabel('t[s]')
+ylabel('pitch-rate [rad/s]')
+
 
 
 
--- trianNNMPC.m
+++ trianNNMPC.m
@@ -8,6 +8,8 @@ if dataSetNumber == 1
     load('MPCdata1.mat')
 elseif dataSetNumber == 2
     load('MPCdata2.mat')
+elseif dataSetNumber == 3
+    load('MPCdata3.mat')
 end 
 
 
@@ -17,7 +19,7 @@ yTrian = y;
 
 dataSet.stateSize = 8; %Size of input state
 dataSet.actionSize = 2;
-dataSet.actionHorizon = 4; %Output horizon of the NN controller
+dataSet.actionHorizon = 2; %Output horizon of the NN controller
 dataSet.X = X;
 dataSet.y = y;
 
@@ -32,7 +34,7 @@ else
 end 
 
 dataSet.validatePercent = 0.05; %Use 5% data to validate
-dataSet.testPercent = 0.05; %Use 10% data to test
+dataSet.testPercent = 0.10; %Use 10% data to test
 prepedData  = prepareData(dataSet);
 
 
@@ -83,8 +85,8 @@ options = trainingOptions('adam', ...
 epochSampleSize = floor(size(prepedData.TrainInput,1)/batchSize);
 epochLossList = zeros(2,epochNum);
 for i = 1:epochNum
-    epochLossList(1,i) = trainInfo.ValidationLoss(1,epochSampleSize*i);
-    epochLossList(2,i) = trainInfo.TrainingLoss(1,epochSampleSize*i);
+    epochLossList(1,i) = trainInfo.TrainingLoss(1,epochSampleSize*i);
+    epochLossList(2,i) = trainInfo.ValidationLoss(1,epochSampleSize*i);
 end
 
 
@@ -95,7 +97,6 @@ plot(1:epochNum,epochLossList(2,:),'Color','#D95319','LineWidth',2)
 legend('Training','Validation')
 xlabel('Epoch')
 ylabel('Loss')
-save('epochLoss.mat','epochLossList')
 
 % RMSE calculation
 predictOutput = predict(MPCNetObj,prepedData.testInput);
@@ -104,59 +105,71 @@ fprintf('Final Test RMS Error = %d\n', testError);
 
 
 
-%%  Result evaluation 
-% We compare the performance and computation time of 
-% 1. The original MPC controller
-% 2. The NN controller
-% 3. The MPC controller initiated by 
-testSize = size(prepedData.testInput,1);
-computationTimeList = zeros(testSize,3);
-costList = zeros(testSize,3);
-for i = 1:testSize
-    % Unload the problem setup:
-    distX =  prepedData.testInput(i,1);
-    distZ =  prepedData.testInput(i,2);
-    initVel = prepedData.testInput(i,3:4)';
-    goalVel = prepedData.testInput(i,5:6)';
-    initPitch =  prepedData.testInput(i,7);
-    initPitchRate = prepedData.testInput(i,8);
-    initState = [0;0;initVel;initPitch;initPitchRate];
-    goalState = [distX;distZ;goalVel];
-
-    % Original MPC Controller
-    commandMPC = reshape(prepedData.testOutput(i,:),[dataSet.actionSize,dataSet.actionHorizon]);
-    
-    if timeRecordFlag
-        % Use the original solver time if it is recorded
-        computationTimeList(i,1) = prepedData.time(i,:);
-    else
-        % Solve the MPC and record the computation time
-        tic;
-        [commandMPC, ~] = droneMPC(dt, horizon, initState, goalState, costParam, quadParam);
-        duration = toc;
-        computationTimeList(i,1) = toc;
-    end
-    costList(i,1) = computeCostRollOut(dt, dataSet.actionHorizon, initState, goalState, commandMPC, costParam, quadParam);
-
-    % NN controller
-    tic;
-    commandNN = double(reshape(predict(MPCNetObj, prepedData.testInput(i,:)),[dataSet.actionSize,dataSet.actionHorizon]));
-    duration = toc; 
-    computationTimeList(i,2) = toc;
-    commandNN = reshape(commandNN, [dataSet.actionSize,dataSet.actionHorizon]);
-    costList(i,2) = computeCostRollOut(dt, dataSet.actionHorizon, initState, goalState, commandNN, costParam, quadParam);
-
-    % MPC primed with NN result. 
-    tic;
-    [commandNNMPC, ~] = droneMPC(dt, horizon, initState, goalState, costParam, quadParam, commandNN);
-    duration = toc; 
-    computationTimeList(i,3) = toc;
-    costList(i,3) = computeCostRollOut(dt, dataSet.actionHorizon, initState, goalState, commandNNMPC, costParam, quadParam);
-end
-
-
-
-
-
-
+% %%  Result evaluation 
+% % We compare the performance and computation time of 
+% % 1. The original MPC controller
+% % 2. The NN controller
+% % 3. The MPC controller initiated by 
+% testSize = size(prepedData.testInput,1);
+% computationTimeList = zeros(testSize,3);
+% costList = zeros(testSize,3);
+% for i = 1:testSize
+%     % Unload the problem setup:
+%     distX =  prepedData.testInput(i,1);
+%     distZ =  prepedData.testInput(i,2);
+%     initVel = prepedData.testInput(i,3:4)';
+%     goalVel = prepedData.testInput(i,5:6)';
+%     initPitch =  prepedData.testInput(i,7);
+%     initPitchRate = prepedData.testInput(i,8);
+%     initState = [0;0;initVel;initPitch;initPitchRate];
+%     goalState = [distX;distZ;goalVel];
+% 
+%     % Original MPC Controller
+%     commandMPC = reshape(prepedData.testOutput(i,:),[dataSet.actionSize,dataSet.actionHorizon]);
+%     
+%     if timeRecordFlag
+%         % Use the original solver time if it is recorded
+%         computationTimeList(i,1) = prepedData.time(i,:);
+%     else
+%         % Solve the MPC and record the computation time
+%         tic;
+%         [commandMPC, ~] = droneMPC(dt, horizon, initState, goalState, costParam, quadParam);
+%         computationTimeList(i,1) = toc;
+%     end
+%     costList(i,1) = computeCostRollOut(dt, dataSet.actionHorizon, initState, goalState, commandMPC, costParam, quadParam);
+% 
+%     % NN controller
+%     tic;
+%     commandNN = double(reshape(predict(MPCNetObj, prepedData.testInput(i,:)),[dataSet.actionSize,dataSet.actionHorizon]));
+%     computationTimeList(i,2) = toc;
+%     commandNN = reshape(commandNN, [dataSet.actionSize,dataSet.actionHorizon]);
+%     costList(i,2) = computeCostRollOut(dt, dataSet.actionHorizon, initState, goalState, commandNN, costParam, quadParam);
+% 
+%     % MPC primed with NN result. 
+%     tic;
+%     [commandNNMPC, ~] = droneMPC(dt, horizon, initState, goalState, costParam, quadParam, commandNN);
+%     duration = toc; 
+%     computationTimeList(i,3) = toc;
+%     costList(i,3) = computeCostRollOut(dt, dataSet.actionHorizon, initState, goalState, commandNNMPC, costParam, quadParam);
+% end
+% 
+% sScoreNN = ones(testSize,1) - abs(costList(:,2) - costList(:,1))./costList(:,1);
+% sScoreNN_MPC = ones(testSize,1) - abs(costList(:,3) - costList(:,1))./costList(:,1);
+% 
+% if dataSetNumber == 1
+%     save('TrainingResult1','computationTimeList','costList','epochLossList','MPCNetObj')
+% elseif dataSetNumber == 2
+%     save('TrainingResult2','computationTimeList','costList','epochLossList','MPCNetObj')
+% elseif dataSetNumber == 3
+%     save('TrainingResult3','computationTimeList','costList','epochLossList','MPCNetObj')
+% end 
+% 
+% fprintf('mean CompTime = %d\n', mean(computationTimeList));
+% fprintf('var CompTime = %d\n', var(computationTimeList));
+% 
+% fprintf('mean S-score NN = %d\n', mean(sScoreNN));
+% fprintf('var S-score NN= %d\n', var(sScoreNN));
+% 
+% fprintf('mean S-score NN_MPC = %d\n', mean(sScoreNN_MPC));
+% fprintf('var S-score NN_MPC= %d\n', var(sScoreNN_MPC));
 
